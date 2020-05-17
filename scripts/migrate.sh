4ce105a7b322ec74f78648bf46d349ad1274d8e4
Emilio Silva Schlenker <emilio@autark.xyz>
2020-05-16 19:04:53 -0500

    removed the mongod initialization and disposal from migrate

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -5,8 +5,6 @@ production="$(heroku config:get MONGODB_URI -a coinosis)"
 testing="$(heroku config:get MONGODB_URI -a coinosis-test)"
 development=mongodb://localhost/coinosis
 
-mongod --dbpath=${currentDir}/../db &
-
 mongodump --uri=${production} -o proddb-backup/ &&
 mongodump --uri=${testing} -o testdb-backup/ &&
 mongodump --uri=${development} -o devdb-backup/ &&
@@ -16,5 +14,3 @@ mongorestore -d coinosis testdb-backup/heroku_7m49q4bs &&
 
 mongo ${testing} ${currentDir}/deleteAll.js &&
 mongorestore --uri=${testing} -d heroku_7m49q4bs proddb-backup/heroku_t2bt9b8m/
-
-pkill mongod

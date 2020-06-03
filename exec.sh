aef0b8907cfa73ff9d8eb450b88a4e73b1bb2f3d
Emilio Silva Schlenker <emilio@autark.xyz>
2020-06-03 12:45:13 -0500

    removed the exec script as it's not very useful

--- exec.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-if [ -z ${MONGODB_URI} ]; then
-    MONGODB_URI=coinosis
-fi
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-mongo ${MONGODB_URI} ${DIR}/${1}

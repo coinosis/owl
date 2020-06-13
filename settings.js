0364e25370236c42fcdc81bb30352fcfc2374493
Emilio Silva Schlenker <e18r@disroot.org>
2020-06-13 07:59:09 -0500

    added a log about the environment on startup

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 const allSettings = require('./settings.json');
 const environment = process.env.ENVIRONMENT || 'development';
+console.log(`running on the ${environment} environment`);
 const settings = allSettings[environment];
 
 module.exports = settings;

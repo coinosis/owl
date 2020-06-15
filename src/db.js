5f5f17804f421e1b8b267e8ef944b6a39d03f222
Emilio Silva Schlenker <e18r@disroot.org>
2020-06-15 15:31:05 -0500

    refactored the db module and corrected the npm test command

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
-const dbURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/coinosis';
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/coinosis';
 
-const dbClient = new MongoClient(dbURL, { useUnifiedTopology: true });
-dbClient.connect();
-const db = dbClient.db();
+const client = new MongoClient(url, { useUnifiedTopology: true });
+client.connect();
+const db = client.db();
 const users = db.collection('users');
 const events = db.collection('events');
 const assessments = db.collection('assessments');
@@ -11,7 +11,7 @@ const payments = db.collection('payments');
 const distributions = db.collection('distributions');
 
 const disconnect = () => {
-  dbClient.close();
+  client.close();
 }
 
 module.exports = {

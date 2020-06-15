b87a4d3dea7afe2ee935dfdb504370ae500f1547
Emilio Silva Schlenker <e18r@disroot.org>
2020-06-15 15:03:32 -0500

    placed all backups in a single backup dir

--- scripts/prod2test.sh
+++ scripts/prod2test.sh
@@ -4,8 +4,8 @@ currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 production="$(heroku config:get MONGODB_URI -a coinosis)"
 testing="$(heroku config:get MONGODB_URI -a testing-owl)"
 
-mongodump --uri=${production} -o ${currentDir}/../proddb-backup/ &&
-mongodump --uri=${testing} -o ${currentDir}/../testdb-backup/ &&
+mongodump --uri=${production} -o ${currentDir}/../backup/prod/ &&
+mongodump --uri=${testing} -o ${currentDir}/../backup/test/ &&
 
 mongo ${testing} ${currentDir}/deleteAll.js &&
-mongorestore --uri=${testing} -d heroku_7m49q4bs ${currentDir}/../proddb-backup/heroku_t2bt9b8m/
+mongorestore --uri=${testing} -d heroku_7m49q4bs ${currentDir}/../backup/prod/heroku_t2bt9b8m/
--- scripts/test2dev.sh
+++ scripts/test2dev.sh
@@ -4,8 +4,8 @@ currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 testing="$(heroku config:get MONGODB_URI -a testing-owl)"
 development=mongodb://localhost/coinosis
 
-mongodump --uri=${testing} -o ${currentDir}/../testdb-backup/ &&
-mongodump --uri=${development} -o ${currentDir}/../devdb-backup/ &&
+mongodump --uri=${testing} -o ${currentDir}/../backup/test/ &&
+mongodump --uri=${development} -o ${currentDir}/../backup/dev/ &&
 
 mongo ${development} ${currentDir}/deleteAll.js &&
-mongorestore -d coinosis ${currentDir}/../testdb-backup/heroku_7m49q4bs
+mongorestore -d coinosis ${currentDir}/../backup/test/heroku_7m49q4bs

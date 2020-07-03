b8963283f8d3274d75d10b31405acf51360a0c4e
Emilio Silva Schlenker <e18r@disroot.org>
2020-07-03 16:43:51 -0500

    build id

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.json('ZdLpo');
+  res.json('TxmJK');
 });
 
 app.get('/eth/price', async (req, res, next) => {

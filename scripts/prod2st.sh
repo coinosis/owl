060f2ef5fd843fc4aa37f648223d1652d3543edd
Emilio Silva Schlenker <e18r@disroot.org>
2020-07-08 11:13:18 -0500

    included staging in the db migration scripts

---
+++ scripts/prod2st.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+production="$(heroku config:get MONGODB_URI -a coinosis)"
+staging="$(heroku config:get MONGODB_URI -a staging-owl)"
+
+mongodump --uri=${production} -o ${currentDir}/../backup/prod/ &&
+mongodump --uri=${staging} -o ${currentDir}/../backup/st/ &&
+
+mongo ${staging} ${currentDir}/deleteAll.js &&
+mongorestore --uri=${staging} -d heroku_0sbhf7ks ${currentDir}/../backup/prod/heroku_t2bt9b8m/
--- scripts/st2test.sh
+++ scripts/st2test.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-production="$(heroku config:get MONGODB_URI -a coinosis)"
+staging="$(heroku config:get MONGODB_URI -a staging-owl)"
 testing="$(heroku config:get MONGODB_URI -a testing-owl)"
 
-mongodump --uri=${production} -o ${currentDir}/../backup/prod/ &&
+mongodump --uri=${staging} -o ${currentDir}/../backup/st/ &&
 mongodump --uri=${testing} -o ${currentDir}/../backup/test/ &&
 
 mongo ${testing} ${currentDir}/deleteAll.js &&
-mongorestore --uri=${testing} -d heroku_7m49q4bs ${currentDir}/../backup/prod/heroku_t2bt9b8m/
+mongorestore --uri=${testing} -d heroku_7m49q4bs ${currentDir}/../backup/st/heroku_0sbhf7ks/

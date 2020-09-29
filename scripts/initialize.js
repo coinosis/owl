eacbad7187b56a51b7105906904568c012fcf27d
Emilio Silva Schlenker <e18r@disroot.org>
2020-09-29 09:34:03 -0500

    refactor initialize script

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -1,9 +1,14 @@
 db.users.createIndex({ address: 1 }, { unique: true });
+
 db.events.createIndex({ url: 1 }, { unique: true });
 db.events.createIndex({ name: 1 }, { unique: true });
+db.events.createIndex({ address: 1 }, { unique: true });
+
 db.series.createIndex({ url: 1 }, { unique: true, });
 db.series.createIndex({ name: 1 }, { unique: true, });
-db.events.createIndex({ address: 1 }, { unique: true });
+
 db.transactions.createIndex({ event: 1, user: 1 }, { unique: true });
+
 db.distributions.createIndex({ event: 1 }, { unique: true });
+
 db.claps.createIndex({ event: 1, clapee: 1 }, { unique: true });
